253e8c1a6658a758213697c5a4238af3a5d8b613
mth <mthdht@gmail.com>
2025-10-06 07:41:39 +0000

    Initial commit from PHPSandbox

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import tailwindcss from 'tailwindcss';
+import { defineConfig, loadEnv } from 'vite';
+import process from 'node:process';
+
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        server: {
+            hmr: {
+                protocol: "wss",
+                host: `${env.SUBDOMAIN}-5173.${env.NOTEBOOK_HOST}`,
+                clientPort: 443,
+            },
+        },
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './resources/js'),
+            },
+        },
+        css: {
+            postcss: {
+                plugins: [tailwindcss, autoprefixer],
+            },
+        },
+    };
+});
